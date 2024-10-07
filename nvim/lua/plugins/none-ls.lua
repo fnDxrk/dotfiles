7f4bb36af403147312bbc8a1146970066ed90497
Philipp Andruschenko <dxrk.rzn7txt@gmail.com>
2024-10-07 11:11:18 +0700

    fix : LSP formatting

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,7 +1,18 @@
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local null_ls = require("null-ls")
 
+local lsp_formatting = function(bufnr)
+    vim.lsp.buf.format({
+        filter = function(client)
+            -- apply whatever logic you want (in this example, we'll only use null-ls)
+            return client.name == "null-ls"
+        end,
+        bufnr = bufnr,
+    })
+end
+
 null_ls.setup({
+    autostart = true,
     sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.completion.spell,
@@ -17,8 +28,7 @@ null_ls.setup({
                 group = augroup,
                 buffer = bufnr,
                 callback = function()
-                    vim.lsp.buf.format({ async = false })
-                    -- vim.lsp.buf.formatting_sync()
+                    lsp_formatting(bufnr)
                 end,
             })
         end
