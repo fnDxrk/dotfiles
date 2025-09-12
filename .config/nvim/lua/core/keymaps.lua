399fbdc9982f00a277b9d11ab344b8fe57b2b37a
Philipp Andruschenko <dxrk.rzn7txt@gmail.com>
2025-09-12 20:15:22 +0700

    nvim: update keymaps

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -5,3 +5,8 @@ vim.keymap.set('n', '<leader>sn', '<cmd>noautocmd w <CR>', opts)
 
 -- Neo-tree --
 vim.keymap.set('n', '<leader>e', ':Neotree filesystem left toggle reveal_force_cwd<CR>', {})
+
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gr", vim.lsp.buf.references)
+vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action)
