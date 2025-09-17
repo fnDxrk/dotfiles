f17284fc3886117035fdc017c46f446019ccaeec
Philipp Andruschenko <dxrk.rzn7txt@gmail.com>
2025-09-17 15:47:34 +0700

    nvim: keymaps for resize

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -17,3 +17,7 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+
+vim.keymap.set('n', '<C-S-h>', ':vertical resize -3<CR>', {})
+vim.keymap.set('n', '<C-S-l>', ':vertical resize +3<CR>', {})
+
