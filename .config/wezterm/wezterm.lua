e7006b011761795cad246c97173ede985d1f3310
Philipp Andruschenko <dxrk.rzn7txt@gmail.com>
2025-09-20 21:16:42 +0700

    wallust: tab bar

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,28 +1,27 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
--- Themes
-config.color_scheme = 'wallust'
-config.color_scheme = "wallust"
+config.use_fancy_tab_bar = false
 
--- config.color_scheme = 
-config.font = wezterm.font('JetBrains Mono NL', { weight = 'Medium' })
+-- Themes
+config.color_scheme = "wallust"
+config.font = wezterm.font("JetBrains Mono NL", { weight = "Medium" })
 config.font_size = 12
 config.window_background_opacity = 0.9
 config.hide_tab_bar_if_only_one_tab = true
 
 -- Disable notification
-config.window_close_confirmation = 'NeverPrompt'
+config.window_close_confirmation = "NeverPrompt"
 
 config.keys = {
   {
-    key = 'w',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.CloseCurrentTab { confirm = false },
+    key = "w",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.CloseCurrentTab({ confirm = false }),
   },
   {
-    key = 'L',
-    mods = 'CTRL|SHIFT',
+    key = "L",
+    mods = "CTRL|SHIFT",
     action = wezterm.action.DisableDefaultAssignment,
   },
 }
