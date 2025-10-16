b3b93983fea724dd2a282d23cc4ff5c87676bbaa
Philipp Andruschenko <dxrk.rzn7txt@gmail.com>
2025-10-16 15:57:42 +0700

    hyprland: multi-gpu script

---
+++ .config/hypr/scripts/multi-gpu.sh
@@ -0,0 +1,33 @@
+# AMD
+SYMLINK_NAME="amd-gpu"
+RULE_PATH="/etc/udev/rules.d/amd-gpu-dev-path.rules"
+AMD_GPU_ID=$(lspci -d ::03xx | grep 'AMD' | cut -f1 -d' ')
+UDEV_RULE="$(cat <<EOF
+KERNEL=="card*", \
+KERNELS=="0000:$AMD_GPU_ID", \
+SUBSYSTEM=="drm", \
+SUBSYSTEMS=="pci", \
+SYMLINK+="dri/$SYMLINK_NAME"
+EOF
+)"
+
+echo "$UDEV_RULE" | sudo tee "$RULE_PATH"
+
+# NVIDIA
+SYMLINK_NAME="nvidia-gpu"
+RULE_PATH="/etc/udev/rules.d/nvidia-gpu-dev-path.rules"
+NVIDIA_GPU_ID=$(lspci -d ::03xx | grep 'NVIDIA' | cut -f1 -d' ')
+UDEV_RULE="$(cat <<EOF
+KERNEL=="card*", \
+KERNELS=="0000:$NVIDIA_GPU_ID", \
+SUBSYSTEM=="drm", \
+SUBSYSTEMS=="pci", \
+SYMLINK+="dri/$SYMLINK_NAME"
+EOF
+)"
+
+echo "$UDEV_RULE" | sudo tee "$RULE_PATH"
+
+# Reload udev rules
+sudo udevadm control --reload
+sudo udevadm trigger
