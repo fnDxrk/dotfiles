4c4a63e2333e53424403e6f0584db2657ad055eb
Philipp Andruschenko <dxrk.rzn7txt@gmail.com>
2025-10-18 19:17:19 +0700

    Script install

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Zsh
+cp .config/zsh/.zshenv ~/.zshenv
+cp -r .config/zsh ~/.config/
+
+# Neovim
+ cp -r .config/nvim ~/.config/
+
+# Electron
+cp .config/electron-flags.conf ~/.config
+
+# Hyprland
+cp -r .config/hypr ~/.config/
+
+# Wezterm
+cp -r .config/wezterm ~/.config/
